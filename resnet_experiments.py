6fbe438a6adf2acb392ed804fa3506b3ea508af5
David Suh <davidsuh@Davids-MacBook-Pro.local>
2023-05-27 22:22:37 -0500

    typo

--- resnet_experiments.py
+++ resnet_experiments.py
@@ -11,7 +11,7 @@ def main(encoder='./checkpoints/model+resnet18.pth', epochs=80):
     train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=4, pin_memory=True)
 
     # Test data
-    test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transformT.ToTensor())
+    test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=T.ToTensor())
     test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=256, shuffle=False, num_workers=4, pin_memory=True)
 
     # get encoder type

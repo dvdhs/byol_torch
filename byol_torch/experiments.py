9a6d863081bbde7de4c4fd3c06082238bfc5730c
David Suh <davidsuh@Davids-MacBook-Pro.local>
2023-05-27 22:54:12 -0500

    experiemnts device cuda

--- byol_torch/experiments.py
+++ byol_torch/experiments.py
@@ -46,7 +46,8 @@ Given an encoder, train a linear classifier on top of it for a given number of e
 then evaluate the performance of the linear classifier on the given test set
 """
 class LinearExperimentationRegime:
-    def __init__(self, encoder, input_dim, output_dim, train_loader, test_loader, epochs=80, lr=1e-3):
+    def __init__(self, encoder, input_dim, output_dim, train_loader, test_loader, epochs=80, lr=1e-3, device=None):
+        self.device = device if device is not None else torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.classifier = LinearClassifier(encoder, input_dim, output_dim)
         self.epochs = epochs
         self.train_loader = train_loader
@@ -58,6 +59,7 @@ class LinearExperimentationRegime:
         for epoch in track(range(self.epochs)):
             tloss = 0
             for x, y in self.train_loader:
+                x,y = x.to(self.device), y.to(self.device)
                 optimizer.zero_grad()
                 logits = self.classifier(x)
                 loss = F.cross_entropy(logits, y)
@@ -72,6 +74,7 @@ class LinearExperimentationRegime:
         total = 0
         with torch.no_grad():
             for x, y in self.test_loader:
+                x,y = x.to(self.device), y.to(self.device)
                 logits = self.classifier(x)
                 _, predicted = torch.max(logits.data, 1)
                 total += y.size(0)

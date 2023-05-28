bf49b8e8663371f517da18bb0e6ae7b498cb4a61
David Suh <davidsuh@Davids-MacBook-Pro.local>
2023-05-27 23:04:22 -0500

    cifar100 support

--- resnet_experiments.py
+++ resnet_experiments.py
@@ -6,15 +6,25 @@ from rich import print
 
 from byol_torch.experiments import LinearExperimentationRegime
 
-def main(encoder_path='./checkpoints/model+resnet18.pth', epochs=80):
-    # Load CIFAR10 dataset
-    train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=T.ToTensor())
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=4, pin_memory=True)
+def main(encoder_path='./checkpoints/model+resnet18.pth', epochs=80, dataset='CIFAR10'):
+    # Load the datasets
+    if dataset == 'CIFAR10':
+        train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=T.ToTensor())
+        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=4, pin_memory=True)
 
-    # Test data
-    test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=T.ToTensor())
-    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=256, shuffle=False, num_workers=4, pin_memory=True)
+        # Test data
+        test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=T.ToTensor())
+        test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=256, shuffle=False, num_workers=4, pin_memory=True)
+    elif dataset == 'CIFAR100':
+        train_dataset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=T.ToTensor())
+        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=4, pin_memory=True)
 
+        # Test data
+        test_dataset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=T.ToTensor())
+        test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=256, shuffle=False, num_workers=4, pin_memory=True)
+    else:
+        raise Exception(f"Unknown dataset provided: {dataset}")
+        
     # get encoder type
     encoder_type = encoder_path.split('+')[1].split('.')[0]
     if encoder_type == 'resnet18':
